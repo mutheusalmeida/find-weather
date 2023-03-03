43f073aa05b3e37a4bde87703a7c60b5d0265fa4
Matheus <matheusalmeida016@gmail.com>
2023-03-03 15:05:22 -0400

    feat: add icons

--- app.tsx
+++ app.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useFonts, Overpass_700Bold } from '@expo-google-fonts/overpass';
+import { useFonts, Overpass_600SemiBold } from '@expo-google-fonts/overpass';
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback } from 'react';
 
@@ -8,7 +8,7 @@ SplashScreen.preventAutoHideAsync()
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Overpass_700Bold,
+    Overpass_600SemiBold,
   });
 
   const onLayoutRootView = useCallback(async () => {
@@ -23,9 +23,9 @@ export default function App() {
 
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={{ fontFamily: 'Overpass_700Bold', fontSize: 36 }}>Hello, World!</Text>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Descubra o Clima na sua Cidade</Text>
+      <Text style={styles.desc}>Com o FindWeather nunca ficou tão fácil ter a previsão do tempo na palma da sua mão</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -33,8 +33,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f4f4f4',
+    backgroundColor: '#1B1D22',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 33,
+    lineHeight: 42,
+    fontFamily: 'Overpass_600SemiBold',
+    maxWidth: 304,
+    textAlign: 'center',
+    marginBottom: 33,
+  },
+  desc: {
+    color: '#AFAFAF',
+    fontSize: 22,
+    lineHeight: 28,
+    fontFamily: 'Overpass_600SemiBold',
+    maxWidth: 360,
+    textAlign: 'center',
+  }
 });
