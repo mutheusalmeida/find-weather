dc1056dc5c96c2c66dd47db5aea223896ee10ef3
Matheus <matheusalmeida016@gmail.com>
2023-03-03 10:54:20 -0400

    feat: add custom fonts

--- app.tsx
+++ app.tsx
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Overpass_700Bold } from '@expo-google-fonts/overpass';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Overpass_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
+      <Text style={{ fontFamily: 'Overpass_700Bold', fontSize: 36 }}>Hello, World!</Text>
       <Text>Hello, World!</Text>
       <StatusBar style="auto" />
     </View>
