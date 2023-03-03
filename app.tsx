3385a76ca98c8a9654c627e5bf1da15f7081f438
Matheus <matheusalmeida016@gmail.com>
2023-03-03 11:06:05 -0400

    feat: await unitl custom font is loaded

--- app.tsx
+++ app.tsx
@@ -1,18 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, Overpass_700Bold } from '@expo-google-fonts/overpass';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Overpass_700Bold,
-  })
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
 
   if (!fontsLoaded) {
     return null
   }
 
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text style={{ fontFamily: 'Overpass_700Bold', fontSize: 36 }}>Hello, World!</Text>
       <Text>Hello, World!</Text>
       <StatusBar style="auto" />
@@ -23,7 +33,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4f4f4',
     alignItems: 'center',
     justifyContent: 'center',
   },
